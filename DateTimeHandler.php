c900cab1591ebb40fe034204ab6ececf7d81c3c8
Patrick Pokatilo <mail@shyxormz.net>
2014-11-07 09:43:48 +0100

    Add handler for deserializing DateTime

---
+++ DateTimeHandler.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SHyx0rmZ\JMSDeYaml;
+
+use JMS\Serializer\DeserializationContext;
+use JMS\Serializer\Exception\RuntimeException;
+use JMS\Serializer\Handler\DateHandler;
+
+class DateTimeHandler extends DateHandler
+{
+    /** @var string */
+    protected $defaultFormat;
+    /** @var string */
+    protected $defaultTimezone;
+
+    public static function getSubscribingMethods()
+    {
+        return array(
+            array(
+                'type' => \DateTime::class,
+                'format' => 'yml',
+                'method' => 'deserialize'
+            )
+        );
+    }
+
+    public function __construct($defaultFormat = \DateTime::ISO8601, $defaultTimezone = 'UTC')
+    {
+        $this->defaultFormat = $defaultFormat;
+        $this->defaultTimezone = $defaultTimezone;
+    }
+
+    public function deserialize(YamlDeserializationVisitor $visitor, $data, array $type, DeserializationContext $context)
+    {
+        if ($data === null) {
+            return null;
+        }
+
+        $format = isset($type['params'][0]) ? $type['params'][0] : $this->defaultFormat;
+        $timezone = isset($type['params'][1]) ? $type['params'][1] : $this->defaultTimezone;
+
+        $datetime = \DateTime::createFromFormat($format, (string)$data, new \DateTimeZone($timezone));
+
+        if (false === $datetime) {
+            throw new RuntimeException(sprintf('Invalid datetime "%s", expected format %s.', $data, $format));
+        }
+
+        return $datetime;
+    }
+}
